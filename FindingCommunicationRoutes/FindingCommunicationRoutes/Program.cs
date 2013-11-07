eb69d0299ee360a49aa6e4a0a6ac9d6e3593c45b
Krzyrok <krzyrok@gmail.com>
2013-11-07 20:25:13 +0100

    deleted test lines

--- FindingCommunicationRoutes/FindingCommunicationRoutes/Program.cs
+++ FindingCommunicationRoutes/FindingCommunicationRoutes/Program.cs
@@ -16,9 +16,6 @@ namespace FindingCommunicationRoutes
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // test
-            // test 2
-            // test 3
         }
     }
 }
