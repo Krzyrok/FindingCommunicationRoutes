41dd7ab000b5b11f8a6ea422997fc35aa70f2dc8
realKojzu <kojzus@o2.pl>
2013-11-09 12:53:04 +0100

    Added the first version of ReaderCHM class.

---
+++ FindingCommunicationRoutes/FindingCommunicationRoutes/ReaderCHM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FindingCommunicationRoutes
+{
+    public class ReaderCHM
+    {
+        private String _filePath;
+
+        private String _outputPath;
+
+        public ReaderCHM(String filePathCHM, String outputPathForStoringHTML)
+        {
+            if (filePathCHM.EndsWith(".chm"))
+            {
+                _filePath = filePathCHM;
+                _outputPath = outputPathForStoringHTML;
+            }
+        }
+
+        public int Decompile()
+        {
+            Process proc = new Process();
+
+            try
+            {
+                proc.StartInfo.FileName = "hh.exe";
+                proc.StartInfo.Arguments = "-decompile " + _outputPath + " " + _filePath;
+                proc.Start();
+                proc.WaitForExit();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return proc.ExitCode;
+        }
+
+        public string[] GetIndexFileFromOutputPath()
+        {
+            if(_outputPath != null)
+                return Directory.GetFiles(_outputPath,"index.html");
+            else
+                return new string[0];
+        }
+    }
+}
