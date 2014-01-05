08b26053074435e70815e157933d86e0f1760441
Krzyrok <krzyrok@gmail.com>
2014-01-05 18:52:32 +0100

    TMP commit for indirect searching.

--- FindingCommunicationRoutes/FindingCommunicationRoutes/Model/Search/SearcherOfIndirectRoutes.cs
+++ FindingCommunicationRoutes/FindingCommunicationRoutes/Model/Search/SearcherOfIndirectRoutes.cs
@@ -60,11 +60,80 @@ namespace FindingCommunicationRoutes
                 }
                 else
                 {
-                    //busStopsToCheckList.Add(new SingleBusStopForIndirectConnection(oneBusStop.BusStopName, startBusStop.BusStopName, singleResult.LineNumber, 
-                    //    singleResult.TimeDistanceBetweenBusStops, singleResult.ArrivalTime));
+                    busStopsToCheckList.Add(new SingleBusStopForIndirectConnection(oneBusStop.BusStopName, startBusStop.BusStopName, singleResult.LineNumber,
+                        singleResult.TimeDistanceBetweenBusStops, singleResult.ArrivalDateTime, singleResult.DepartureDateTime));
                 }
             }
+
+            busStopsToCheckTmpList.Clear();
             // end initialize
+
+            // main fragments
+            do
+            {
+                busStopsToCheckTmpList.AddRange(busStopsToCheckList);
+                busStopsToCheckList.Clear();
+
+                int indexOfTheNearestBusStop = -1;
+                TimeOfArrival actualTotalTimeTravel = new TimeOfArrival(23, 59);
+                for (int i = 0; i < busStopsToCheckTmpList.Count; i++)
+                {
+                    if (busStopsToCheckTmpList[i].TotalTimeFromStartBus == null)
+                    {
+                        continue;
+                    }
+                    if (busStopsToCheckTmpList[i].TotalTimeFromStartBus < actualTotalTimeTravel)
+                    {
+                        indexOfTheNearestBusStop = i;
+                        actualTotalTimeTravel = busStopsToCheckTmpList[i].TotalTimeFromStartBus;
+                    }
+                }
+                // probably route doesn;t exist
+                if (indexOfTheNearestBusStop == -1)
+                {
+                    break;
+                }
+                // one of the conditions
+                if (busStopsToCheckTmpList[indexOfTheNearestBusStop].BusStopName.Equals(soughtConnection.EndBusStop))
+                {
+                    busStopsCheckedList.Add(busStopsToCheckTmpList[indexOfTheNearestBusStop]);
+                    busStopsToCheckTmpList.RemoveAt(indexOfTheNearestBusStop);
+                    busStopsToCheckList.AddRange(busStopsToCheckTmpList);
+                    break;
+                }
+
+                startBusStop = busStopsToCheckTmpList[indexOfTheNearestBusStop];
+                busStopsToCheckTmpList.RemoveAt(indexOfTheNearestBusStop);
+                foreach (SingleBusStopForIndirectConnection oneBusStop in busStopsToCheckTmpList)
+                {
+                    SoughtConnection tmpSoughtConnection = new SoughtConnection(startBusStop.BusStopName, oneBusStop.BusStopName,
+                        startBusStop.ArrivalTimeAtThisBusStop, true);
+                    SearchResultConnection singleResult = searcherOfDirectConnections.FindDirectConnection(repository, tmpSoughtConnection);
+
+                    if (singleResult != null)
+                    {
+                        TimeOfArrival newArrivalTimeOnEndBusStop = new TimeOfArrival(singleResult.ArrivalDateTime.Hour, singleResult.ArrivalDateTime.Minute);
+                        if (oneBusStop.TotalTimeFromStartBus == null)
+                        {
+                            // po prostu zapisz
+                            oneBusStop.ChangeFields(startBusStop.BusStopName, singleResult.LineNumber, 
+                                startBusStop.TotalTimeFromStartBus + singleResult.TimeDistanceBetweenBusStops, singleResult.ArrivalDateTime, singleResult.DepartureDateTime);
+                        }
+                        else
+                        {
+                            // sprawdz czy nowy czss jest mniejszy i zapisz wtedy
+                            if ((startBusStop.TotalTimeFromStartBus + singleResult.TimeDistanceBetweenBusStops) < oneBusStop.TotalTimeFromStartBus) // condition
+                            {
+                                oneBusStop.ChangeFields(startBusStop.BusStopName, singleResult.LineNumber, 
+                                    startBusStop.TotalTimeFromStartBus + singleResult.TimeDistanceBetweenBusStops, singleResult.ArrivalDateTime, singleResult.DepartureDateTime);
+                            }
+                        }
+                    }
+                }
+
+            } while (true);
+
+            // end main fragments
 
             // -----------
 
